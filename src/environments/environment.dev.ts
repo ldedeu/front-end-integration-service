9054cb59b04d501fe28d9b3bf00f63814bca0d73
Lauren Dedeu Aguilera <35501549+ldedeu@users.noreply.github.com>
2022-03-25 17:54:57 -0300

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 // TODO: Remove secrets from this file
 export const environment = {
   production: false,
-  azure_endpoint: 'http://integrationservicenowqa.azurewebsites.net/api/IntegrationConsumer'
+  azure_endpoint: 'https://integrationservicenowqa.azurewebsites.net/api/IntegrationConsumer'
   
   //azure_endpoint: 'http://127.0.0.1:8000/all'
 };
